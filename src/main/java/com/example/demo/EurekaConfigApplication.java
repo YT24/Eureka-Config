f69c0715b53a1eea8d9b16895f6c91faea4ab7ad
yangte66 <yangt@paraview.cn>
2019-09-06 16:43:44 +0800

    123

--- src/main/java/com/example/demo/EurekaConfigApplication.java
+++ src/main/java/com/example/demo/EurekaConfigApplication.java
@@ -10,6 +10,8 @@ public class EurekaConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaConfigApplication.class, args);
+		
+		System.err.println("--------------eureka-------------");
 	}
 
 }
